6aeb09b92fe15ba67beaf56db2628bf266b8663b
HellBr3ad <162746517+HellBr3ad@users.noreply.github.com>
2024-04-26 19:58:42 +0200

    Add files via upload
    Simple To-Do List application

---
+++ Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TodoListApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tasksListBox = new System.Windows.Forms.ListBox();
+            this.taskTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tasksListBox
+            // 
+            this.tasksListBox.FormattingEnabled = true;
+            this.tasksListBox.Location = new System.Drawing.Point(13, 13);
+            this.tasksListBox.Name = "tasksListBox";
+            this.tasksListBox.Size = new System.Drawing.Size(396, 420);
+            this.tasksListBox.TabIndex = 0;
+            // 
+            // taskTextBox
+            // 
+            this.taskTextBox.Location = new System.Drawing.Point(415, 16);
+            this.taskTextBox.Name = "taskTextBox";
+            this.taskTextBox.Size = new System.Drawing.Size(373, 20);
+            this.taskTextBox.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(415, 39);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(183, 23);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Add Task";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(604, 39);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(184, 23);
+            this.removeButton.TabIndex = 3;
+            this.removeButton.Text = "Remove Task";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.taskTextBox);
+            this.Controls.Add(this.tasksListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox tasksListBox;
+        private System.Windows.Forms.TextBox taskTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button removeButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TodoListApp2
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.AcceptButton = addButton;
+            this.ActiveControl = taskTextBox;
+            tasksListBox.ScrollAlwaysVisible = true;
+            tasksListBox.IntegralHeight = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Delete && tasksListBox.Focused)
+            {
+                removeButton_Click(this, new EventArgs());
+                return true;
+            }
+
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            // Check if the taskTextBox is not empty
+            if (!string.IsNullOrWhiteSpace(taskTextBox.Text))
+            {
+                tasksListBox.Items.Add("• " + taskTextBox.Text);
+                taskTextBox.Clear();
+                // Reset the border color to the default (optional)
+                taskTextBox.BorderStyle = BorderStyle.FixedSingle;
+            }
+            else
+            {
+                // Set the border color to red to indicate an error
+                taskTextBox.BorderStyle = BorderStyle.Fixed3D;
+                taskTextBox.BackColor = Color.LightCoral;
+
+                // Optionally, you could set a Timer to clear the color after a few seconds
+                var clearErrorTimer = new Timer();
+                clearErrorTimer.Interval = 1500; // 2 seconds
+                clearErrorTimer.Tick += (s, args) =>
+                {
+                    taskTextBox.BackColor = SystemColors.Window; // Reset back color to default
+                    clearErrorTimer.Stop(); // Stop the timer
+                };
+                clearErrorTimer.Start();
+            }
+            taskTextBox.Focus(); // Puts focus back on the taskTextBox after adding or attempting to add an item
+        }
+
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (tasksListBox.SelectedIndex != -1)
+            {
+                tasksListBox.Items.RemoveAt(tasksListBox.SelectedIndex);
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to remove.", "No Task Selected", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            taskTextBox.Focus();
+        }
+
+
+
+    }
+}
